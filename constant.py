2ea4dcdc6e1bd48d6ae562d20ec7efc0ac7680b3
tong shen <ts325@duke.edu>
2022-04-17 16:31:05 -0700

    refactor code

---
+++ constant.py
@@ -0,0 +1,8 @@
+WORLD_HOST = "vcm-24846.vm.duke.edu"
+WORLD_PORT = 23456
+MSG_LEN = 1024
+UPS_HOST = "vcm-24846.vm.duke.edu"
+UPS_PORT = 34567
+TRUCK_NUM = 8
+RETRY_INTERVAL = 0.01  # sec
+CONNECTED = "connected!"
--- ups.py
+++ ups.py
@@ -1,24 +1,49 @@
 import socket
 import world_ups_pb2
 from google.protobuf.internal.encoder import _EncodeVarint
+from constant import *
 
-HOST = "vcm-24846.vm.duke.edu"
-PORT = 23456
-MSG_LEN = 1024
+def encode_msg(fd, msg):
+    msg_str = msg.SerializeToString()
+    _EncodeVarint(fd.sendall, len(msg_str), None)
+    return msg_str
+
+# return world_id
+def connect_world(world_fd):
+    # ups send UConnect to world
+    u_connect = world_ups_pb2.UConnect()
+    u_connect.isAmazon = False
+    for i in range(TRUCK_NUM):
+        u_connect.trucks.add(id=i, x=i, y=i)
+    msg_str = encode_msg(world_fd, u_connect)
+    world_fd.sendall(msg_str)
+    # ups receive UConnected from world
+    msg = world_fd.recv(MSG_LEN)
+    u_connected = world_ups_pb2.UConnected()
+    u_connected.ParseFromString(msg[1:])  # first byte is length
+    world_id = u_connected.worldid
+    result = u_connected.result
+    if result != CONNECTED:
+        print("error: {}".format(result))
+    print("world id: {}".format(world_id))
+    return world_id
+
+def send_world_id(amazon_fd):
+    return
+
+
+
+def main():
+    world_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    world_fd.connect((WORLD_HOST, WORLD_PORT))
+    connect_world(world_fd)
+
+    # listen_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # listen_fd.bind(UPS_HOST, UPS_PORT)
+    # listen_fd.listen(1)
+    # amazon_fd, _ = listen_fd.accept()
+
+
 
 if __name__ == "__main__":
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as world_fd:
-        world_fd.connect((HOST, PORT))
-
-        u_connect = world_ups_pb2.UConnect()
-        u_connect.isAmazon = False
-        u_connect.trucks.add(id=1,x=1,y=1) # add one truck
-        u_connect_msg = u_connect.SerializeToString()
-        _EncodeVarint(world_fd.sendall, len(u_connect_msg), None)
-        world_fd.sendall(u_connect_msg)
-
-        msg = world_fd.recv(MSG_LEN)
-        u_connected = world_ups_pb2.UConnected()
-        u_connected.ParseFromString(msg[1:])  # first byte is length
-        print("world id: {}".format(u_connected.worldid))
-        print("result: {}".format(u_connected.result))
+    main()
