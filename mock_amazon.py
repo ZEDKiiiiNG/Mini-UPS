5a0536ad506dbfd3b7dc0af52be468d57596d9d3
tong shen <ts325@duke.edu>
2022-04-18 10:28:05 -0700

    rm extra code

--- mock_amazon.py
+++ mock_amazon.py
@@ -5,15 +5,6 @@ import time
 
 
 def test_send_world_id(ups_fd):
-    # u_msg = ups.recv_msg(ups_fd, amazon_ups_pb2.UMsg)
-    # print(u_msg)
-    # #  does not reply ack
-    # u_msg = ups.recv_msg(ups_fd, amazon_ups_pb2.UMsg)
-    # print(u_msg)
-    # # time.sleep(5)
-    # # sec_msg = ups_fd.recv(MSG_LEN)
-    # # print(sec_msg)
-    # # time.sleep(5)
     count = 10
     for _ in range(count):
         u_msg = ups.recv_msg(ups_fd, amazon_ups_pb2.UMsg)
