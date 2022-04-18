ad775c1d755d01604e570bc69b5eb99a844fda2f
tong shen <ts325@duke.edu>
2022-04-18 00:39:59 -0700

    u_msg.worldid is a list

--- mock_amazon.py
+++ mock_amazon.py
@@ -3,16 +3,16 @@ import ups
 import amazon_ups_pb2
 
 
-def main():
+def test_send_world_id():
     ups_fd = ups.build_client(UPS_HOST, UPS_PORT)
     u_msg = ups.recv_msg(ups_fd, amazon_ups_pb2.UMsg)
-    print(u_msg)
-    world_id = u_msg.worldid.worldid
-    seq = u_msg.worldid.seqnum
-    print("world id: {}".format(world_id))
-    print("seq: {}".format(seq))
+    for w in u_msg.worldid:
+        world_id = w.worldid
+        seq = w.seqnum
+        print("world id: {}".format(world_id))
+        print("seq: {}".format(seq))
     return
 
 
 if __name__ == "__main__":
-    main()
+    test_send_world_id()
