e1551b4fceb4dc88938698b6a251ada1dd2956f7
ZEDKIIIING <296213663@qq.com>
2022-04-19 15:38:51 -0400

    Complete the Django and rebanse

--- ups.py
+++ ups.py
@@ -6,7 +6,7 @@ from constant import *
 import amazon_ups_pb2
 import time
 import select
- 
+
 
 def send_msg(fd, msg):
     msg_str = msg.SerializeToString()
@@ -219,4 +219,18 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as world_fd:
+        world_fd.connect((HOST, PORT))
+
+        u_connect = world_ups_pb2.UConnect()
+        u_connect.isAmazon = False
+        u_connect.trucks.add(id=1,x=1,y=1) # add one truck
+        u_connect_msg = u_connect.SerializeToString()
+        _EncodeVarint(world_fd.sendall, len(u_connect_msg), None)
+        world_fd.sendall(u_connect_msg)
+
+        msg = world_fd.recv(MSG_LEN)
+        u_connected = world_ups_pb2.UConnected()
+        u_connected.ParseFromString(msg[1:])  # first byte is length
+        print("world id: {}".format(u_connected.worldid))
+        print("result: {}".format(u_connected.result))
