2277611840a1555ba41172de25c6d5d6d60f9dda
tong shen <ts325@duke.edu>
2022-04-19 23:06:30 -0700

    write for test

--- mock_world.py
+++ mock_world.py
@@ -7,6 +7,7 @@ def test_completion(ups_fd):
     w_msg =  world_ups_pb2.UResponses()
     w_msg.completions.add(truckid=2, x=3, y=4, status=ARRIVE_WAREHOUSE, seqnum=40)
     send_msg(ups_fd, w_msg)
+    recv_ack(ups_fd)
     return
 
 
@@ -14,7 +15,6 @@ def main():
     listen_fd = build_server(WORLD_HOST, WORLD_PORT)
     ups_fd, _ = listen_fd.accept()
     test_completion(ups_fd)
-    recv_ack(ups_fd)
     while True:
         pass
     return
