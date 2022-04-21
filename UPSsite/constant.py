6532cc4879eaf1c811c910f73b5ae70e19d6b0d6
tong shen <ts325@duke.edu>
2022-04-20 22:00:25 -0700

    move

---
+++ UPSsite/constant.py
@@ -0,0 +1,22 @@
+WORLD_HOST = "vcm-24846.vm.duke.edu"
+WORLD_PORT = 12345
+MSG_LEN = 1024
+UPS_HOST = "vcm-24846.vm.duke.edu"
+UPS_PORT = 34567
+TRUCK_NUM = 8
+RETRY_INTERVAL = 5  # sec
+CONNECTED = "connected!"
+
+# truck status
+IDLE = "idle"
+TRAVELING = "traveling"
+ARRIVE_WAREHOUSE = "arrive warehouse"
+LOADING = "loading"
+DELIVERING = "delivering"
+
+# package status
+CREATED = "created"
+TRUCK_EN_ROUTE_TO_WAREHOUSE = "truck en route to warehouse"
+TRUCK_WAITING_FOR_PACKAGE = "truck waiting for package"
+OUT_FOR_DELIVERY = "out for delivery"
+DELIVERED = "delivered"
