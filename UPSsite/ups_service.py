58b56b8d9997530b70d85a55df581b9389753294
tong shen <ts325@duke.edu>
2022-04-22 22:25:41 -0700

    handle amazon close connection

--- UPSsite/ups_service.py
+++ UPSsite/ups_service.py
@@ -287,7 +287,7 @@ def run_service(amazon_fd, curr_seq, exp_seqs, amazon_ack_seqs, world_ack_seqs):
         ready_fds, _, _ = select.select([world_fd, amazon_fd], [], [], 0)
         if amazon_fd in ready_fds:
             a_msg = recv_msg(amazon_fd, amazon_ups_pb2.AMsg)
-            if not a_msg:  # amazon close connection
+            if not a_msg:
                 print("amazon close connection")
                 return
             print("receive a_msg: {}".format(a_msg))
