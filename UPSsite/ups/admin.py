99c87cc2f64fc5e16d9e972dc1e558ce545247fc
tong shen <ts325@duke.edu>
2022-04-24 14:43:23 -0700

    mv

--- UPSsite/ups/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from . import models
-admin.site.register(models.User)
-admin.site.register(models.Truck)
-admin.site.register(models.Package)
-admin.site.register(models.Product)
